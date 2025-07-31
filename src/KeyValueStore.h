5459aed28e33d52a09dd036a8945a63020d090d8
AnshBalar429 <anshbalar2910@gmail.com>
2025-07-31 18:08:54 +0530

    Add data_map and constructure

--- src/KeyValueStore.h
+++ src/KeyValueStore.h
@@ -1,6 +1,15 @@
 #ifndef KEY_VALUE_STORE_H
 #define KEY_VALUE_STORE_H
+#include <string>
+#include <unordered_map>
 
+class KeyValueStore {
+private:
+    // The main underlying data structure to store the key-value pair
+    std::unordered_map<std::string, std::string> data_map;
 
+public:
+    KeyValueStore();
+};
 
 #endif // KEY_VALUE_STORE_H
