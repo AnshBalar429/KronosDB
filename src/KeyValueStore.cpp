4b38f3e4f7eb8598e45971502256fc6b7d4bf882
AnshBalar429 <anshbalar2910@gmail.com>
2025-07-31 19:18:45 +0530

    implement set and get

--- src/KeyValueStore.cpp
+++ src/KeyValueStore.cpp
@@ -1,5 +1,22 @@
 #include "KeyValueStore.h"
+#include <iostream>
 
-KeyValueStore::KeyValueStore() {
+KeyValueStore::KeyValueStore() = default;
 
+void KeyValueStore::set(const std::string& key, const std::string& value) {
+    // if key exists it reassigns or insert
+    data_map.insert_or_assign(key, value);
+    std::cout << "SET: " << key << " = " << value << std::endl;
+}
+
+std::optional<std::string> KeyValueStore::get(const std::string& key) {
+    auto it = data_map.find(key);
+
+    if (it != data_map.end()) {
+        std::cout << "GET: " << key << " -> " << it->second << std::endl;
+        return it->second;
+    } else {
+        std::cout << "GET: " << key << " -> (not found)" << std::endl;
+        return std::nullopt; // empty optional
+    }
 }
