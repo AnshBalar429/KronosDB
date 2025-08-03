4e18b280aadaa10a49f4bac7f6dce1c788176ac1
AnshBalar429 <anshbalar2910@gmail.com>
2025-08-03 18:44:17 +0530

    Impl process_command

--- src/Session.cpp
+++ src/Session.cpp
@@ -2,13 +2,60 @@
 #include <iostream>
 #include <istream>
 #include <ostream>
+#include <utility>
 
-Session::Session(boost::asio::ip::tcp::socket socket) : socket_(std::move(socket)) {}
+Session::Session(boost::asio::ip::tcp::socket socket, std::shared_ptr<KeyValueStore> store)
+    : socket_(std::move(socket)) , store_(std::move(store)) {}
 
 void Session::start() {
     std::cout << "New session started from" << socket_.remote_endpoint() << std::endl;
     do_read();
 }
+
+std::string Session::process_command(const std::string& line) {
+    std::stringstream ss(line);
+    std::string command;
+    ss >> command;
+
+    if (command == "GET") {
+        std::string key;
+        ss >> key;
+        if (key.empty()) {
+            return "ERROR : Key not specified\n";
+        }
+
+        const auto value = store_->get(key);
+
+        return value.has_value() ? *value + "\n" : "(nil)\n";
+
+    } else if (command == "SET") {
+        std::string key, value;
+        ss >> key;
+
+        // The rest of the line will be the value
+        std::getline(ss, value);
+
+        if (!value.empty() && value[0] == ' ') {
+            value.erase(0, 1);
+        }
+
+        if (key.empty() || value.empty()) {
+            return "ERROR : SET requires a key and value\n";
+        }
+
+        store_->set(key, value);
+        return "OK\n";
+
+    } else if (command == "DEL") {
+        std::string key;
+        ss >> key;
+        if (key.empty()) return "ERROR: Key not specified\n";
+
+        store_->del(key);
+        return "DELETED\n";
+    }
+}
+
 
 void Session::do_read() {
     auto self(shared_from_this());
