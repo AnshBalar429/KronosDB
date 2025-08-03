3e427637dcffe2f043d1e5a4f147ea218e106136
AnshBalar429 <anshbalar2910@gmail.com>
2025-08-03 18:17:25 +0530

    Implement do_accept

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,5 +1,6 @@
 #include "Server.h"
 #include <iostream>
+#include "Session.h"
 
 Server::Server(unsigned short port)
     : acceptor_(io_context_, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {
@@ -8,5 +9,20 @@ Server::Server(unsigned short port)
 
 void Server::run() {
     std::cout << "Server is starting..." << std::endl;
+    do_accept();
     io_context_.run();
+}
+
+
+void Server::do_accept() {
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket) {
+            if (!ec) {
+                std::make_shared<Session>(std::move(socket))->start();
+            } else {
+                std::cerr << "Accept Error : " << ec.message() << std::endl;
+            }
+            do_accept();
+        }
+    );
 }
