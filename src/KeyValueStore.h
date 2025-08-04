661faad2c144753406c6118ff80529c9259eaed6
AnshBalar429 <anshbalar2910@gmail.com>
2025-08-04 18:01:13 +0530

    Add persistence

--- src/KeyValueStore.h
+++ src/KeyValueStore.h
@@ -4,6 +4,9 @@
 #include <unordered_map>
 #include <optional>
 #include <shared_mutex>
+#include <memory>
+
+#include "Persistence.h"
 
 class KeyValueStore {
 private:
@@ -16,8 +19,12 @@ private:
     // so we should make mutex mutable so it can be used inside const function.
     // Here const function is KeyValueStore::get()
 
+    std::unique_ptr<Persistence> persistence_;
+
+    void load_from_aof(const std::string& filename);
+
 public:
-    KeyValueStore();
+    KeyValueStore(const std::string& aof_filename);
 
     void set(const std::string& key, const std::string& value);
 
